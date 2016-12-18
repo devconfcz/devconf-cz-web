6facb18f3ad09f32736f2224c66c2a7716926495
Daniel Passos <daniel@passos.me>
2016-12-18 20:18:25 -0200

    Improve show/hide speaker bio

--- js/speakers.js
+++ js/speakers.js
@@ -1,6 +1,6 @@
 $(function () {
     initializeFirebase();
-    speaker();
+    speakers();
 });
 
 function initializeFirebase() {
@@ -15,29 +15,43 @@ function initializeFirebase() {
     firebase.initializeApp(config);
 }
 
-// --
+// -- Speakers --------------------------------------------------------------------------------------------------------
 
-function speaker() {
+function speakers() {
 
     // -- HTML Triggers -----------------------------------------------------------------------------------------------
 
 
+    $("body")
+        // Show/Hide speaker bio when click on speaker photo
+        .on("click", ".card-image", function (event) {
+            event.stopImmediatePropagation();
+
+            $(this).parent().find('.card-content').slideToggle();
+        })
+        // Hide speaker bio when click on speaker bio
+        .on("click", ".card-content", function (event) {
+            event.stopImmediatePropagation();
+
+            $(this).slideToggle();
+        });
+
     // -- Firebase Database Triggers ----------------------------------------------------------------------------------
 
     var speakersRef = firebase.database().ref().child("speakers");
-    
-    speakersRef.orderByChild("name").on('child_added', function _add(snap, prevChild) {
-        addNewSpeakerCard(snap.val(), snap.key);
+
+    speakersRef.orderByChild("name").on('child_added', function _add(snapshot) {
+        addNewSpeakerCard(snapshot.val(), snapshot.key);
     });
 
     // -- Helper methods ----------------------------------------------------------------------------------------------
 
     function addNewSpeakerCard(speaker, id) {
-        var html = "<div class='speaker speakers-page card hoverable'>" +
-            "<div class='card-image' sid=\"" + id + "\" style='background-image: url(\"" + speaker.avatar + "\")'>" +
+        var html = "<div class='speaker card hoverable " + id + "'>" +
+            "<div class='card-image' style='background-image: url(\"" + speaker.avatar + "\")'>" +
             "<span class='card-title'>" + speaker.name + "</span>" +
             "</div>" +
-            "<div class='card-content' id=\"" + id + "\" hide='yes'>" +
+            "<div class='card-content'>" +
             speaker.bio +
             "</div>";
         // Card Actions
@@ -50,30 +64,7 @@ function speaker() {
         // html += "</div>";
 
         $('.speakers-container').append(html);
-
-	clickInit();
     }
-}
-
-function clickInit(){
-    $('.card-image').click(function(e){
-	e.stopImmediatePropagation();
-	var speakerCard = $(this).attr('sid');
-	if($("#"+speakerCard).attr('hide') == 'yes'){
-		$("#"+speakerCard).slideDown();
-		$("#"+speakerCard).attr('hide','no');			
-	}else{
-		$("#"+speakerCard).slideUp();
-                $("#"+speakerCard).attr('hide','yes');
-	}
-    });
-
-    $('.card-content').click(function(e){
-	e.stopImmediatePropagation();
-        $(this).slideUp();
-        $(this).attr('hide','yes');
-    });
-
-    
 
 }
+
