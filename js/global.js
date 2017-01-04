519f2edc9d6224bceee7ffb70ab3c4622063dff3
Daniel Passos <daniel@passos.me>
2017-01-04 21:08:27 -0200

    Speakers details in a modal

---
+++ js/global.js
@@ -0,0 +1,18 @@
+$(function () {
+    initializeFirebase();
+
+    // http://materializecss.com/modals.html#initialization
+    $('.modal').modal();
+});
+
+function initializeFirebase() {
+    var config = {
+        apiKey: "AIzaSyBw1XF-Jbkz3DV8mANU_SQgYYq-wErVZfQ",
+        authDomain: "devconf-cz-2017.firebaseapp.com",
+        databaseURL: "https://devconf-cz-2017.firebaseio.com",
+        storageBucket: "devconf-cz-2017.appspot.com",
+        messagingSenderId: "88312300184"
+    };
+
+    firebase.initializeApp(config);
+}
--- js/speakers.js
+++ js/speakers.js
@@ -1,46 +1,42 @@
 $(function () {
-    initializeFirebase();
     speakers();
 });
 
-function initializeFirebase() {
-    var config = {
-        apiKey: "AIzaSyBw1XF-Jbkz3DV8mANU_SQgYYq-wErVZfQ",
-        authDomain: "devconf-cz-2017.firebaseapp.com",
-        databaseURL: "https://devconf-cz-2017.firebaseio.com",
-        storageBucket: "devconf-cz-2017.appspot.com",
-        messagingSenderId: "88312300184"
-    };
-
-    firebase.initializeApp(config);
-}
-
 function speakers() {
+
+    // Speakers local cache
+    var speakers = [];
 
     // -- Firebase Database Triggers ----------------------------------------------------------------------------------
 
-    var speakersRef = firebase.database().ref().child("speakers");
+    var speakersRef = firebase.database().ref().child("speakers").orderByChild("name");
 
-    speakersRef.orderByChild("name").on('child_added', function (snapshot) {
-        addNewSpeakerCard(snapshot.key, snapshot.val());
-    });
+    speakersRef.once('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            var speaker = childSnapshot.val();
+            speakers[speaker.id] = speaker;
+        });
 
-    speakersRef.on('child_changed', function (snapshot) {
-        updateSpeakerCard(snapshot.key, snapshot.val());
-    });
+        displaySpeakers();
 
-    speakersRef.on('child_removed', function (snapshot) {
-        removeSpeakerCard(snapshot.key, snapshot.val());
+        if(window.location.hash) {
+            var speakerId = window.location.hash.replace("#", "");
+            if(speakers[speakerId]) {
+                $(window.location.hash)[0].scrollIntoView();
+                showSpeakerDetails(speakerId);
+            }
+        }
+
     });
 
     // -- HTML Triggers -----------------------------------------------------------------------------------------------
 
     $("body")
-    // Show/Hide speaker bio when click on speaker photo
+    // Display speakers details
         .on("click", ".card-image.speaker-image", function (event) {
             event.stopImmediatePropagation();
 
-            $(this).parent().find('.card-content.speaker-bio').slideToggle();
+            showSpeakerDetails($(this).parent().attr("id"));
         })
         // Hide speaker bio when click on speaker bio
         .on("click", ".card-content.speaker-bio", function (event) {
@@ -51,14 +47,12 @@ function speakers() {
 
     // -- Helper methods ----------------------------------------------------------------------------------------------
 
-    /**
-     * An id to be used in HTML to indentify the speaker
-     *
-     * @param id Database speakers id
-     * @returns {string} Speakers Id
-     */
-    function createSpeakerId(id) {
-        return "speaker-" + id;
+    function displaySpeakers() {
+        for(id in speakers) {
+            addNewSpeakerCard(speakers[id]);
+        }
+
+        $(".preloader-wrapper").addClass("hide");
     }
 
     /**
@@ -67,41 +61,35 @@ function speakers() {
      * @param id Speaker ID
      * @param speaker speaker instance
      */
-    function addNewSpeakerCard(id, speaker) {
-        var html = "<div id='" + createSpeakerId(id) + "' class='card speaker hoverable'>" +
+    function addNewSpeakerCard(speaker) {
+        var html = "<div id='" + speaker.id + "' class='card speaker hoverable'>" +
             "<div class='card-image speaker-image' style='background-image: url(\"" + speaker.avatar + "\")'>" +
             "<span class='card-title speaker-name'>" + speaker.name + "</span>" +
-            "</div>" +
-            "<div class='card-content speaker-bio'>" +
-            speaker.bio +
             "</div>";
 
         $('.speakers-container').append(html);
     }
 
-    /**
-     * Update a spekaer card with the new informations changed in the database
-     *
-     * @param id Speaker id
-     * @param speaker Speaker instance
-     */
-    function updateSpeakerCard(id, speaker) {
-        var speakerCard = $("#" + createSpeakerId(id));
-        var speakerAvatar = speakerCard.find(".speaker-image");
-        var speakerName = speakerCard.find(".speaker-name");
-        var speakerBio = speakerCard.find(".speaker-bio");
+    function showSpeakerDetails(speakerId) {
+        var speaker = speakers[speakerId];
+        var modal = $('#speaker-detail');
 
-        speakerAvatar.css('background-image', 'url(' + speaker.avatar + ')');
-        speakerName.text(speaker.name);
-        speakerBio.text(speaker.bio);
-    }
+        modal.find(".speaker-image").attr("src", speaker.avatar);
+        modal.find(".speaker-name").text(speaker.name);
+        modal.find(".speaker-country").text(speaker.country);
+        modal.find(".speaker-organization").text(speaker.organization);
+        modal.find(".speaker-bio").text(speaker.bio);
 
-    function removeSpeakerCard(id, speaker) {
-        var speakerCard = $("#" + createSpeakerId(id));
+        var twitterHTML = "";
+        if (speaker.twitter) {
+            twitterHTML = "<a href='http://twitter.com/" + speaker.twitter + "' target='_blank' class='twitter'>" +
+                "@" + speaker.twitter + "" +
+                "</a>";
 
-        speakerCard.fadeOut(500, function () {
-            speakerCard.remove();
-        });
+        }
+        modal.find(".speaker-twitter").html(twitterHTML);
+
+        modal.modal('open');
     }
 
 }
